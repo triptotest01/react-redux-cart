db2725e77aa8afe38469a633d33357e89715ceab
triptotest01 <triptotest01@gmail.com>
2023-04-15 13:24:49 +0900

    리덕스 cart 쇼핑몰 구현

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,18 @@
+import Card from '../UI/Card';
+import classes from './Cart.module.css';
+import CartItem from "./CartItem";
+
+
+const Cart = (props) => {
+  return(
+     <Card className={classes.cart}>
+        <h2> Your Shopping Cart</h2>
+        <ul>
+          <CartItem item={{title: 'A Book', quantity:3, total:15, price:5}}/>
+        </ul>
+     </Card> 
+  )
+}
+
+
+export default Cart;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import {Fragment} from 'react'
+import MainHeader from "./MainHeader";
+
+
+const Layout =(props) =>{
+
+return(
+  <Fragment>  
+    <MainHeader/>
+  </Fragment>
+
+)
+
+}
+
+export default Layout;
